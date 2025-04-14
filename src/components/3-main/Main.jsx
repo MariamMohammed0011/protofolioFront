0d771ac34194c41cb372b0a0b21d5a3f03e76c56
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-14 03:39:46 -0700

    22

--- src/components/3-main/Main.jsx
+++ src/components/3-main/Main.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState ,useRef} from "react";
 import "../3-main/Main.css";
 // import { myProjects } from "./myProjects";
 import axios from "axios";
