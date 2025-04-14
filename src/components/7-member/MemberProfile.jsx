92c485fb20c8b107672f5dcdd7288b7baead3059
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-04-14 04:42:26 -0700

    22

--- src/components/7-member/MemberProfile.jsx
+++ src/components/7-member/MemberProfile.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { OurGroup } from "../4-group/OurGroup"; 
 import Lottie from "lottie-react";
 import { AnimatePresence, motion } from "motion/react";
-import pro from "../../animations/pro.json"; 
+import loading from "../../animations/loading2.json"; 
 import "./MemberProfile.css"; 
 import Contact from "../5-contact/Contact";
 import Footer from "../6-footer/Footer";
@@ -14,53 +14,31 @@ import axios from "axios";
 export default function MemberProfile() {
   
   const [currentActive, setCurrentActive] = useState("all");
- 
-const [allProjects, setAllProjects] = useState([]);
-  // const { id } = useParams(); 
-  // const member = OurGroup.find((member) => member.id === parseInt(id));
+  const [allProjects, setAllProjects] = useState([]);
   const { id } = useParams();
   const [member, setMember] = useState(null);
   const [projects, setProjects] = useState([]);
   const [currentCategory, setCurrentCategory] = useState("all");
   const lottieRef =useRef();
-  // if (!member) {
-  //   return <h1>Member Not Found</h1>;
-  // }
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    if (!id) return; // avoid firing when id is undefined
-  
+   
     axios
-      .get(`https://protofolioback-production.up.railway.app/api/users/${id}`, {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        timeout: 5000,
-      })
+      .get(`https://protofolioback-production.up.railway.app/api/users/${id}`)
       .then((response) => {
         if (response.data.data) {
           setMember(response.data.data);
           setProjects(response.data.data.projects);
+          setIsLoading(false); 
         }
       })
       .catch((error) => {
         console.error("Error fetching member data:", error);
+        setIsLoading(false); 
       });
   }, [id]);
 
-  // if (!member) {
-  //   return <h1>Member Not Found</h1>;
-  // }
-
-  // const memberProjects=member.projects;
-  // const [arr, setArr] = useState(memberProjects);
-  // const filterProjects = (category) => {
-  //   setCurrentCategory(category);
-  //   if (category === "all") {
-  //     setProjects(member.projects);
-  //   } else {
-  //     setProjects(member.projects.filter((p) => p.category.includes(category)));
-  //   }
-  // };
 
   const handleClick = (buttonCategory) => {
     setCurrentActive(buttonCategory);
@@ -76,153 +54,7 @@ const [allProjects, setAllProjects] = useState([]);
 
   
   return (
-    // <div className="member">
-    // <section className=" hero flex" >
-    //   <div className="left-section ">
-    //     <div className="parent-avatar flex">
-    //       <motion.img
-    //         src={member.imagePath} 
-    //         initial={{ transform: "scale(0)" }}
-    //         animate={{ transform: "scale(1.1)" }}
-    //         transition={{ type: "spring", stiffness: 100, damping: 6 }}
-    //         className="user-image avatar"
-    //       />
-    //       <div className="icon-verified"></div>
-    //     </div>
-    //     <motion.h1
-    //       initial={{ opacity: 0 }}
-    //       animate={{ opacity: 1 }}
-    //       transition={{ duration: 2 }}
-    //       className="title"
-    //     >
-    //       {member.name} - {member.work} 
-    //     </motion.h1>
-    //     <p className="subtitle">
-    //       {member.bio || "This member is part of our amazing team!"}
-    //     </p>
-    //     <div className="social-icons flex">
-    //       <div className="icon icon-facebook"></div>
-    //       <div className="icon icon-linkedin2"></div>
-    //       <div className="icon icon-twitter"></div>
-    //       <div className="icon icon-github"></div>
-    //     </div>
-    //   </div>
-    //   <div className="right-section animation" >
-    //     <Lottie animationData={pro} style={{ width: "500px" }} />
-    //   </div>
-      
-     
-    // </section>
-    // <div className="divider" />
-  
-    // <main className="flex ">
-    //   <section className=" flex  left-section">
-    //     <button
-    //       className={currentActive === "all" ? "active" : ""}
-    //       onClick={() => {
-    //         setCurrentActive("all");
-    //         setArr(memberProjects);
-    //       }}
-    //     >
-    //       All Projects
-    //     </button>
-    //     <button
-    //       className={currentActive === "css" ? "active" : ""}
-    //       onClick={() => {
-    //         handleClick("css");
-    //       }}
-    //     >
-    //       {" "}
-    //       HTML & CSS
-    //     </button>
-    //     <button
-    //       className={currentActive === "flutter" ? "active" : ""}
-    //       onClick={() => {
-    //         handleClick("flutter");
-    //       }}
-    //     >
-    //       {" "}
-    //       Flutter
-    //     </button>
-    //     <button
-    //       className={currentActive === "react" ? "active" : ""}
-    //       onClick={() => {
-    //         handleClick("react");
-    //       }}
-    //     >
-    //       {" "}
-    //       React & MUI
-    //     </button>
-    //     <button
-    //       className={currentActive === "node" ? "active" : ""}
-    //       onClick={() => {
-    //         handleClick("node");
-    //       }}
-    //     >
-    //       {" "}
-    //       Node & JavaScript
-    //     </button>
-    //   </section>
-    //   <section className=" flex right-section  ">
-    //     <AnimatePresence>
-    //     {arr.map((item) => {
-    //       return (
-    //         <motion.article key={item.imgPath} className="card  "
-    //         layout
-    //         initial={{transform:"scale(0)"}}
-    //         animate={{transform:"scale(1)"}}
-    //         transition={{type:"spring", damping:8,stiffness:50}}
-    //         >
-            
-    //           <img
-    //             src={item.imgPath}
-    //            width={240}
-    //             alt=""
-    //             className="image-projects "
-    //           />
-             
-            
-    //           <div className="box  " 
-    //           style={{ width: "240px" }}
-    //           >
-    //             <h1 className="title">{item.projectTitle}</h1>
-    //             <p className="subtitle">
-    //               Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    //               Tenetur
-    //             </p>
-    //             <div className="flex icons ">
-    //               <div 
-    //               // style={{ gap: "11px" }}
-    //                className="flex">
-    //                 <div className="icon-link"></div>
-    //                 <div className="icon-github"></div>
-    //               </div>
-
-    //               <a href="" className="link flex">
-    //                 more
-    //                 <span
-    //                   style={{ alignSelf: "end" }}
-    //                   className="icon-arrow-right2"
-    //                 ></span>
-    //               </a>
-    //             </div>
-    //           </div>
-    //         </motion.article>
-    //       );
-    //     })}
-    //     </AnimatePresence>
-      
-    //   </section>
-    // </main>
-  
-
-    
-    
-    // <div className="divider" />
-    // <Contact/>
-    // <div className="divider" />
-    // <Footer/>
-    // </div>
+   
     <div className="member">
       <section className="hero flex">
         <div className="left-section">
@@ -265,6 +97,17 @@ const [allProjects, setAllProjects] = useState([]);
       </section>
 
         <section className="right-section flex">
+          {isLoading ? (
+              <Lottie 
+                lottieRef={lottieRef} 
+                onLoadedImages={() => { lottieRef.current.setSpeed(1) }}
+                animationData={loading} 
+                style={{ width: "300px" }}
+              />
+            ) : (
+              <>
+                {error && <div className="error-message">{error}</div>}
+                
             {projects.map((project) => (
                       <motion.article key={project.id} className="card"
                         layout initial={{ transform: "scale(0)" }}
@@ -285,6 +128,8 @@ const [allProjects, setAllProjects] = useState([]);
                         </div>
                       </motion.article>
                     ))}
+                     </>
+  )}
           
         </section>
       </main>
