ab49944a74e310b3c66f2a4eae3394b65a00784e
MariamMohammed0011 <maream.mohamad.0938856942@gmail.com>
2025-05-13 05:15:18 -0700

    22

--- src/components/4-group/Group.jsx
+++ src/components/4-group/Group.jsx
@@ -1,47 +1,22 @@
-import React, { useState ,useEffect,useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import loading from '../../animations/loading2.json'
+import loading from "../../animations/loading2.json";
 import Lottie from "lottie-react";
-import "./Group.css"; 
+import "./Group.css";
 // import { OurGroup } from "./OurGroup";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-const teamMembers = [
-  {
-    name: "Stefan Mikic",
-    role: "Head of Development",
-    image: "/images/stefan.jpg",
-    linkedin: "#",
-    github: "#",
-  },
-  {
-    name: "Marko Vukic",
-    role: "Head of Development",
-    image: "/images/marko.jpg",
-    linkedin: "#",
-    github: "#",
-  },
-  {
-    name: "Tamara Ristic",
-    role: "Project Manager",
-    image: "/images/tamara.jpg",
-    linkedin: "#",
-    github: "#",
-  },
-];
 export default function Group() {
-  
   const lottieRef =useRef();
-  
+
   const [isLoading, setIsLoading] = useState(true);
 
-  // const [group] = useState(OurGroup);
   const [group, setGroup] = useState([]);
   const [error, setError] = useState(null);
   useEffect(() => {
@@ -60,8 +35,26 @@ export default function Group() {
   }, []);
 
   return (
+    
+<div className='teaming'>
 
-<div className="team">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   {isLoading ? (
       <Lottie 
         lottieRef={lottieRef} 
@@ -69,20 +62,21 @@ export default function Group() {
         animationData={loading} 
         style={{ width: "300px" }}
       />
-    ) : (
+    ) :
+     (
       <>
-        {error && <div className="error-message">{error}</div>}
+        {error && <div className="error-message">{error}</div>} 
 
-<div className="team-section">
-      <h2 className="team-title">Our experienced team</h2>
-      <p className="team-subtitle">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
-      </p>
-   <Swiper
-    modules={[Navigation, Pagination, Autoplay]}
+
+
+
+
+
+<Swiper
+    modules={[ Pagination, Autoplay]}
     spaceBetween={400} 
     slidesPerView={1}
-    navigation
+    // navigation
     pagination={{ clickable: true, dynamicBullets: true }}
     // autoplay={{ delay: 2500, disableOnInteraction: false }}
     breakpoints={{
@@ -90,30 +84,86 @@ export default function Group() {
       768: { slidesPerView: 2, spaceBetween: 30 },
       1024: { slidesPerView: 3, spaceBetween: 30 },
     }}
-      className="swiper-container"
+      className="slide-continer "
     >
-    {group.map((user) => (
-      <SwiperSlide key={user.id} className="swiper-slide">
-        <div className="card-list">
-          <div className="card-item">
+ 
+ 
+ 
+ 
+ 
+ 
+ {
+ 
+ 
+ 
+ 
+ group.map((user) => (
+  <SwiperSlide key={user.id} className="slide-content">
+       
+
+<div className='cardd'>
+<div className='image-content'>
+<span className='overlay '></span>
+<div className='card-image '>
             <Link to={`/member/${user.id}`} className="member-link">
-              <img src={user.profile_picture} alt={user.name} className="user-image" />
-              <h2 className="user-name">{user.name}</h2>
-              <p className="user-profession">{user.specialization}</p>
+              <img src={user.profile_picture} alt={user.name} className="card-img" />
+             
             </Link>
-            <button className="message-button">Message</button>
+
+
+</div>
+</div>
+
+  <div className='card-content '>
+  <h1 className='name'>{user.name}</h1>
+  <p className='description'>{user.specialization}</p>
+  <button className='button-view-more'>view more</button>
+</div>
+
+
+
+
+</div>
+
+
+
+
+
             
-          </div>
-        </div>
+        
+       
       </SwiperSlide>
-    ))}
-  </Swiper> 
+    ))
+    
+    
+    
+    
+    
+    }
 
 
-</div>
-  
-  </>
-  )}
-</div>
+      </Swiper>
+
+</>
+
+
+
+
+
+
+
+      )}
+
+
+
+
+
+
+
+
+
+  </div>
+ 
+
   );
 }
